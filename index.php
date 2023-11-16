54e444b5ae436a9b3892407572bdc48346b88940
legalito <63124952+legalito@users.noreply.github.com>
2023-11-16 15:44:08 +0100

    convert json to csv

---
+++ index.php
@@ -0,0 +1,40 @@
+
+	
+<form enctype='multipart/form-data' action='' method='post'>
+		
+<label>Upload Product CSV file Here</label>
+ 
+<input size='50' type='file' name='filename' accept=".csv">
+</br>
+<input type='submit' name='submit' value='Upload Products' >
+ 
+</form>
+<?php
+$filename = "";
+if (isset($_POST['submit'])) 
+{
+    $row =[];
+    $filename = basename($_FILES['filename']['name'],".csv");
+    $file = fopen($_FILES['filename']['tmp_name'], "r");
+    while (($row = fgetcsv($file)) !== false) {
+        $rows[] = $row;
+    }
+    fclose($file);
+    // Remove the first one that contains headers
+    $headers = array_shift($rows);
+    // Combine the headers with each following row
+    $array = [];
+    foreach ($rows as $row) {
+        $array[] = array_combine($headers, $row);
+    }
+    $json = json_encode($array,JSON_PRETTY_PRINT);
+    file_put_contents($filename.'.json',$json);
+
+    }
+?>
+<?php if($filename !== ""){
+    ?>
+    <p>Télécharge ici le nouveau fichier en json <a href="./<?= $filename ?>.json" download="./<?= $filename ?>.json"><?= $filename ?>.json</a></p>
+
+    <?php
+}?>
